dcdc13ae2dbca4e44248ae1eadd660a16242bbeb
turakakarthik <turakakarthik553@gmail.com>
2025-08-13 23:09:23 +0530

    SmallestLetterGreaterthanTarget With small change done on 13-08-2025

--- BinarySearchPatternsAndQuestions/SmallestLetterGreaterthanTarget.java
+++ BinarySearchPatternsAndQuestions/SmallestLetterGreaterthanTarget.java
@@ -13,18 +13,18 @@ public class SmallestLetterGreaterthanTarget {
     }
     public static char nextGreatestChar(char[] letters,char target)
     {
-        int start=0,end=letters.length-1;
+       int start=0,end=letters.length-1;
         int mid;
         while(start<=end)
         {
             mid=start+(end-start)/2;
-            if(letters[mid]<target)
+            if(target < letters[mid])
             {
-                start=mid+1;
+                end=mid-1;
             }
             else
             {
-                end=mid-1;
+                start=mid+1;
             }
         }
         return letters[start%letters.length];
