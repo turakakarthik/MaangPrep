344389e06b6a3dc80b3c1757c2d613eadb3b3269
turakakarthik <turakakarthik553@gmail.com>
2025-08-13 22:20:13 +0530

    CeilingOfANumber done on 13-08-2025

---
+++ BinarySearchPatternsAndQuestions/CeilingOfANumber.java
@@ -0,0 +1,32 @@
+package BinarySearchPatternsAndQuestions;
+
+public class CeilingOfANumber {
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,8,12,16,21};
+        int target=15;
+        int idx = ceiling(arr, target);
+        System.out.println("Ceiling of target element "+target+" is "+arr[idx]);
+    }
+    static int ceiling(int arr[],int target)
+    {
+        int start=0,end=arr.length-1;
+        int mid;
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            if(arr[mid]==target)
+            {
+               return mid;
+            }
+            else if(target>arr[mid])
+            {
+                start=mid+1;
+            }
+            else
+            {
+                end=mid-1;
+            }
+        }
+        return start;
+    }
+}
