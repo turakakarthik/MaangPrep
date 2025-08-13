2636eb7a0d846762e4ac8f2e39ceaa871c8fc5cc
turakakarthik <turakakarthik553@gmail.com>
2025-08-13 22:51:29 +0530

    SmallestLetterGreaterthanTarget done on 13-08-2025

---
+++ BinarySearchPatternsAndQuestions/SmallestLetterGreaterthanTarget.java
@@ -0,0 +1,32 @@
+package BinarySearchPatternsAndQuestions;
+//744.https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
+public class SmallestLetterGreaterthanTarget {
+    public static void main(String[] args) {
+        char[] arr1 = {'c','f','j'};
+        char target1 = 'a';
+        char ans1 = nextGreatestChar(arr1,target1);
+        System.out.println("smallest element greater than target "+target1+" is "+ans1);
+        char[] arr2 = {'a','b'};
+        char target2 = 'z';
+        char ans2 = nextGreatestChar(arr2,target2);
+        System.out.println("smallest element greater than target "+target2+" is "+ans2);
+    }
+    public static char nextGreatestChar(char[] letters,char target)
+    {
+        int start=0,end=letters.length-1;
+        int mid;
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            if(letters[mid]<target)
+            {
+                start=mid+1;
+            }
+            else
+            {
+                end=mid-1;
+            }
+        }
+        return letters[start%letters.length];
+    }
+}
