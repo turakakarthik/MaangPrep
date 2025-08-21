81c082e3e4c01363314a64e5832f230563f359e4
turakakarthik <turakakarthik553@gmail.com>
2025-08-21 23:25:07 +0530

    FindAllDuplicatesinanArray done on 21-08-2025

---
+++ CyclicSortPatternQuestions/FindAllDuplicatesinanArray.java
@@ -0,0 +1,41 @@
+package CyclicSortPatternQuestions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllDuplicatesinanArray {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> al = new ArrayList<>();
+        cyclicSort(nums);
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=i+1)
+            {
+                al.add(nums[i]);
+            }
+        }
+        return al;
+    }
+    static void cyclicSort(int arr[])
+    {
+        int i=0;
+        while(i<arr.length)
+        {
+            int correct = arr[i]-1;
+            if(arr[i]==arr[correct])
+            {
+                i++;
+            }
+            else
+            {
+                swap(arr,i,correct);
+            }
+        }
+    }
+    static void swap(int arr[],int idx1,int idx2)
+    {
+        int tmp = arr[idx1];
+        arr[idx1]=arr[idx2];
+        arr[idx2]=tmp;
+    }
+}
